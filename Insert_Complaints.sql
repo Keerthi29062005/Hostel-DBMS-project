84261c218812928debf02c58be7ad556c35de485
Kammula Keerthi <128952654+Keerthi29062005@users.noreply.github.com>
2024-07-07 15:04:09 +0530

    Create Insert_Complaints.sql

---
+++ Insert_Complaints.sql
@@ -0,0 +1,6 @@
+INSERT INTO complaints VALUES('cb01','22ECB0B41','PB','8808','LHB','Tap Leakage','PB1',false);
+INSERT INTO complaints VALUES('cb02','20CHB0C45','EC','4307','4blk','Fan slow','EC1',true);
+INSERT INTO complaints VALUES('cb03','20ECB0B27','CL','3225','hst2','Room cleaning','CL1',true);
+INSERT INTO complaints VALUES('cb04','20CEB0B15','CP','B101','hst1','Chair broken','CP1',true);
+INSERT INTO complaints VALUES('cg05','21CHB0B31','LA','1216','LHA','Lan Port broken','LA1',false);
+INSERT INTO complaints VALUES('cg06','22ECB0B25','EC','1312','LHC','Light not working','EC2',false);
